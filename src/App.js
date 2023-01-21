825398665f26eecb3c2963f8a61fa4fc5f24b370
Dilanean <dilaneanarman@gmail.com>
2023-01-21 11:37:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Uomo from './pages/Uomo/Uomo';
 import Cart from './pages/cart/Cart';
